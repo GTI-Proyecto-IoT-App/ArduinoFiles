ea5aaae9e0be44437c052ab8b172d1d4833204f7
JuanC99 <juancarloshr123@gmail.com>
2021-01-17 20:09:59 +0100

    100% operativo codigo arduino en ESP32

--- ConjuntoBasuraArduino/ConjuntoBasuraArduino.ino
+++ ConjuntoBasuraArduino/ConjuntoBasuraArduino.ino
@@ -1,136 +1,64 @@
 #include <WiFiManager.h>
-
-
-//#include <ESP8266WiFi.h>
-//#include <ESP8266WebServer.h>
-
-
-
-
-//#include <SoftwareSerial.h>
-//#include <WiFi.h>
-//#include  < ESP8266WiFi.h>  
-//#include  < DNSServer.h >  
-//#include  < ESP8266WebServer.h >  
-//#include  < WiFiManager.h >
-#include "basuraInteligente.h"
 #include <PubSubClient.h>
 #include "HX711.h"
 
-// paramtros final de carrera
+// parametros final de carrera
 #define PinGPIOInterruptFinalCarrera 2
 bool haCalculadoLlenado = false;
 
 // parametros sensor supersonico
 const double cmTopeBasura =30.0; // tamaño de la basura
 const double cmDistanciaSensorDeBasura = 2.0; // tamaño de la basura
-//const int valorCalibracion=5; 
+
 const uint8_t EchoPin = 17; // receptor sensor
 const uint8_t TriggerPin = 16;//emisor sensor
 
+WiFiManager wifiManager;
+//-----------------WIFI-RouterPortatil-------------
+//const char* WifiSSID = "TP-Link_6FEE";
+//const char* wifiPass = "90821950";
 
-//const char* WifiSSID = "TP-Link_701E";//"TP-Link_6FEE";
-//const char* wifiPass = "28333401";
-
-/////////////////////////////////////////////////////const char* WifiSSID = "MiFibra-EA9E";
-/////////////////////////////////////////////////////const char* wifiPass = "bggtPfp9";
-
-
-
-
-
-
-//const char* WifiSSID = "angle_triangle";
-//const char* wifiPass = "12345678";
+//------------------WIFI_CASA-------------
+//const char* WifiSSID = "MiFibra-EA9E";
+//const char* wifiPass = "bggtPfp9";
 
 // MQTT
-// Add your MQTT Broker IP address, example:
-//const char* mqtt_server = "192.168.1.144";
 const char* mqtt_server = "broker.hivemq.com";
 const char* rootMqttClient = "proyectoGTI2A/dispositivo/";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// bascula
+HX711 bascula;
+//bascula
 const int DOUT=4;
 const int CLK=5;
 
-HX711 bascula;
 
-//inicializacion de la basura
-BasuraInteligente basura;
 
 void setup() {
 
   // put your setup code here, to run once:
   Serial.begin(true,false,true);
-  Serial.begin(115200);//antes tenia este valor 9600 ---------**--
-
-
-
-
-//WiFiManager
- //Local intialization. Once its business is done, there is no need to keep it around
- WiFiManager wifiManager;
- //reset saved settings
- wifiManager.resetSettings();
+  Serial.begin(115200);
  
- //set custom ip for portal
- //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
- //fetches ssid and pass from eeprom and tries to connect
- //if it does not connect it starts an access point with the specified name
- //here "AutoConnectAP"
- //and goes into a blocking loop awaiting configuration
- wifiManager.autoConnect("AutoConnectAP");
- //or use this for auto generated name ESP + ChipID
- //wifiManager.autoConnect();
- 
- //if you get here you have connected to the WiFi
- Serial.println("connected...yeey :)");
-
-
- 
-
-  
-
-   // set up wifi y mqtt
-//  setup_wifi();
+  //set up wifi y mqtt
+  setup_wifi();
   client.setServer(mqtt_server, 1883);
-
-
-  // set up BASURA
-
-//  ContenedorInteligente listaContenedores[4];
-//  listaContenedores[0] = ContenedorInteligente(17,16,12,30,20,0.5,"Vidrio"); 
-//  listaContenedores[1] = ContenedorInteligente(EchoPin,TriggerPin,12,cmTopeBasura,20,0.5,"Plastico"); 
-//  listaContenedores[2] = ContenedorInteligente(EchoPin,TriggerPin,12,cmTopeBasura,20,0.5,"Organico"); 
-//  listaContenedores[3] = ContenedorInteligente(EchoPin,TriggerPin,12,cmTopeBasura,20,0.5,"Papel"); 
-
-  //BasuraInteligente basura(id,&listaContenedores[0]);
-
+  //config supersonico
   pinMode(TriggerPin, OUTPUT);
   pinMode(EchoPin, INPUT);
 
-  // balanza
-  Serial.println("Bascula");
-    bascula.begin(DOUT, CLK);
-    bascula.read();
-    //bascula.set_scale(439430.25); // Establecemos la escala
-    bascula.set_scale(-199764);
-    //bascula.tare(20);  //El peso actual es considerado Tara.
-  Serial.println("END Bascula");
+  // Config bascula
+  bascula.begin(DOUT, CLK);
+  bascula.read();
+  //calibracion
+  bascula.set_scale(-199764);
   // set up final de carrera
   pinMode(PinGPIOInterruptFinalCarrera, INPUT);
-  
   attachInterrupt(digitalPinToInterrupt(PinGPIOInterruptFinalCarrera),final_carrera_activado,HIGH);
-
-
-  
   if(isBasuraAbierta()){
     haCalculadoLlenado = true;
   }
-
 }
 
 
@@ -142,26 +70,19 @@ void final_carrera_activado(){
  
   haCalculadoLlenado = false;
 }
-/*
+
 void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.printf("Connecting to %s ", WifiSSID);
-  WiFi.begin(WifiSSID, wifiPass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(" CONNECTED");
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+ //reset saved settings
+ //wifiManager.resetSettings();
+ //nombre del punto de acceso
+ wifiManager.autoConnect("Angle_Project");
+ //Si llega a este punto se ha conectado correctamente
+ Serial.println("Se ha conectado correctamente");
 }
 
-**
- * public un mensaje al servidor mqtt indicandole el topic
- */
+
+
+//publica un mensaje al servidor mqtt indicandole el topic 
 void publicarMqttAlTopic(String topic,String payload){
   
   char charBufPayload[payload.length() + 1];
@@ -182,20 +103,16 @@ void loop() {
   client.loop();
   
   if(!haCalculadoLlenado && !isBasuraAbierta()){
-
-     // porcentajeLlenado = calcularLlenado(TriggerPin,EchoPin);
-     String id = WiFi.macAddress()+"%basura";
-      publicarMqttAlTopic(id+"/mesuras",calcularBasura(id));
-      haCalculadoLlenado = true;
-     //enviarInfoUart(basura.calcular());
-
-     delay(1000);
-     //dormirM5Stack();
+    
+    String id = WiFi.macAddress()+"%basura";
+    publicarMqttAlTopic(id+"/mesuras",calcularBasura(id));
+    haCalculadoLlenado = true;
+    delay(1000);
+    //dormirM5Stack();
    }else{
     Serial.println("Esta calculando o esta abierta");
    }
-
-  delay(500);
+   delay(500);
 }
 
 void reconnect() {
@@ -232,13 +149,11 @@ String calcularContenedor(String tipo){
   
   String res = "";
   
-  
   res = String(res + "\"tipo\":"+ "\""+tipo+"\",");
   res = String(res + "\"volumen\":"+calcularLlenado()+",");
   res = String(res + "\"peso\":"+calcularPeso()); 
-
+  Serial.println(res);
   return res;
-  
 };
 
 /**
@@ -247,29 +162,17 @@ String calcularContenedor(String tipo){
 String calcularBasura(String id){
 
   String jsonMessage = "{\"id\":";
-
   
-
-   jsonMessage = String(jsonMessage + "\"" + id + "\",");
-    jsonMessage = String(jsonMessage+"\"medidas\": [");
-    jsonMessage = String(jsonMessage+"{"+ calcularContenedor("vidrio")+ "},");
-    //jsonMessage = String(jsonMessage+"{"+ calcularContenedor("papel")+ "},");
-    //jsonMessage = String(jsonMessage+"{"+ calcularContenedor("organico")+ "},");
-    jsonMessage = String(jsonMessage+"{"+ calcularContenedor("plastico")+ "}");
-//  JsonObject  medidasObjVid = medidas.createNestedObject();
-//  JsonObject  medidasObjVid = medidas.createNestedObject();
-//  JsonObject  medidasObjVid = medidas.createNestedObject();
-//  JsonObject  medidasObjVid = medidas.createNestedObject();
-    jsonMessage = String(jsonMessage+"]}");
-//   medidasObjVid["vidrio"] = calcularContenedor();
-//   medidasObj["organico"] = calcularContenedor();
-//   medidasObj["papel"] = calcularContenedor();
-//   medidasObj["plastico"] = calcularContenedor();
-   
-   
-
+  jsonMessage = String(jsonMessage + "\"" + id + "\",");
+  jsonMessage = String(jsonMessage+"\"medidas\": [");
+  jsonMessage = String(jsonMessage+"{"+ calcularContenedor("vidrio")+ "},");
+  //jsonMessage = String(jsonMessage+"{"+ calcularContenedor("papel")+ "},");
+  //jsonMessage = String(jsonMessage+"{"+ calcularContenedor("organico")+ "},");
+  jsonMessage = String(jsonMessage+"{"+ calcularContenedor("plastico")+ "}");
+  jsonMessage = String(jsonMessage+"]}");
+  
   return jsonMessage;
-};
+}
 
 
 /**
@@ -282,23 +185,19 @@ String calcularBasura(String id){
 double calcularLlenado() {
   
   int valorCalibracion = 5;
-
+  
   double duracion,porcentajeLlenadoBasura,distanciaCm;
-  //Serial.println((*this).triggerPinUltrasonico);
-  digitalWrite(TriggerPin/*(*this).triggerPinUltrasonico*/, LOW); //nos aseguramos señal baja
+  digitalWrite(TriggerPin, LOW);
   delayMicroseconds(4);
-
-  digitalWrite(TriggerPin/*(*this).triggerPinUltrasonico*/, HIGH); //generamos pulso de 10us
+  
+  digitalWrite(TriggerPin, HIGH); //generamos pulso de 10us
   delayMicroseconds(10);
-  digitalWrite(TriggerPin/*(*this).triggerPinUltrasonico*/, LOW);
-
-  duracion = pulseIn(EchoPin/*(*this).echoPinUltrasonico*/, HIGH); //medimos el tiempo pulso
-
+  digitalWrite(TriggerPin, LOW);
+  duracion = pulseIn(EchoPin, HIGH); //medimos el tiempo pulso
   
   distanciaCm = duracion * 10.0 / 292.0 / 2.0; //convertimos a distancia
-  distanciaCm = distanciaCm - cmDistanciaSensorDeBasura;//(*this).distanciaContenedor ;
-  porcentajeLlenadoBasura = 100.0 - (distanciaCm/cmTopeBasura/*(*this).profundidadContenedor*/ )*100.0;//conversión a %
-   
+  distanciaCm = distanciaCm - cmDistanciaSensorDeBasura;
+  porcentajeLlenadoBasura = 100.0 - (distanciaCm/cmTopeBasura)*100.0;//conversión a %
   //calibracion
   if(porcentajeLlenadoBasura > (100-valorCalibracion)){
     porcentajeLlenadoBasura=100.0;
